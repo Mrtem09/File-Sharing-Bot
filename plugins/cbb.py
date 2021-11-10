362b6ece438073da76ea601607b14baa5dd6d617
MR TEM <92890384+Mrtem09@users.noreply.github.com>
2021-11-10 05:01:19 +0200

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Editor : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Series : <a href='https://t.me/cinema2_bot/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/mc_developers'>Click here</a>\n○ Channel : @c0nnect3dMovieS\n○ Support Group : @mc_movie_requests</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
